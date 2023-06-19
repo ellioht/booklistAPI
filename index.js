6cf042ad2c333060d2aa0d23397de9d8553338df
Elliot <130664947+ellioht@users.noreply.github.com>
2023-06-19 15:15:09 +0100

    prt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const router = require('./router.js');
 
 // Tell the Express application to parse incoming JSON data
