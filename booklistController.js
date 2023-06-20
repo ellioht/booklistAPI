66954815043b00533257282ee6a707f60af70e00
Elliot <130664947+ellioht@users.noreply.github.com>
2023-06-20 13:11:33 +0100

    implemented mongodb with mongoose

--- booklistController.js
+++ booklistController.js
@@ -1,84 +1,119 @@
-const createError = require('http-errors');
+const createError = require("http-errors");
+const { Book } = require("./bookModel.js");
 
 // sample books
-var books = [
-    { id: 1, title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', read: true },
-    { id: 2, title: 'Harry Potter and the Philosopher\'s Stone', author: 'J.K. Rowling', read: false },
-    { id: 3, title: 'The Hobbit', author: 'J.R.R. Tolkien', read: true },
-    { id: 4, title: 'And Then There Were None', author: 'Agatha Christie', read: false },
-];
+// let books = [
+//   { id: 1, title: "The Lord of the Rings", author: "J.R.R. Tolkien", read: true },
+//   { id: 2, title: "Harry Potter and the Philosopher's Stone", author: "J.K. Rowling", read: false },
+//   { id: 3, title: "The Hobbit", author: "J.R.R. Tolkien", read: true },
+//   { id: 4, title: "And Then There Were None", author: "Agatha Christie", read: false },
+// ];
 
 // GET /api/books
-exports.getBooks = (req, res) => {
-    res.status(200).json(books);
-}
+exports.getBooks = async (req, res, next) => {
+  // res.status(200).json(books);
+  try {
+    const books = await Book.find();
+    res.send(books);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
 
 // GET /api/books/:id
-exports.getBookById = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const book = books.find(book => book.id === id);
-    if (book) {
-        res.status(200).json(book);
-    } else {
-        next(createError(404));
+exports.getBookById = async (req, res, next) => {
+  if (!req.params.id) {
+    return next(createError(400, "Missing required fields"));
+  }
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return next(createError(404, "Book not found"));
     }
-}
+    res.send(book);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
 
 // POST /api/books
-exports.addBook = (req, res) => {
-    const book = req.body;
-    book.id = books.length + 1;
-    books.push(book);
-    res.status(201).json(book);
-}
+exports.addBook = async (req, res, next) => {
+  if (!req.body.title) {
+    return (next(createError(400, "Missing required fields")));
+  }
+  try {
+    const newBook = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      read: req.body.read,
+    });
+    await newBook.save();
+    res.send(newBook);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
 
 // PUT /api/books/:id
-exports.updateBook = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const book = books.find(book => book.id === id);
-    if (book) {
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.read = req.body.read;
-        res.status(200).json(book);
-    } else {
-        next(createError(404));
-    }
-}
+exports.updateBook = async (req, res, next) => {
+  if (!req.params.id) {
+    return next(createError(400, "Missing required fields"));
+  }
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      author: req.body.author,
+      read: req.body.read,
+    });
+
+    res.send(updatedBook);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
 
 // DELETE /api/books/:id
-exports.deleteBook = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const book = books.find(book => book.id === id);
-    if (book) {
-        books = books.filter(book => book.id !== id);
-        res.status(200).json(book);
-    } else {
-        next(createError(404));
+exports.deleteBook = async (req, res, next) => {
+  if (!req.params.id) {
+    return next(createError(400, "Missing required fields"));
+  }
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) {
+      return next(createError(404, "Book not found"));
     }
-}
+    res.send(deletedBook);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
 
 // DELETE all books
-exports.deleteBooks = (req, res) => {
-    books = [];
-    res.status(200).json(books);
-}
+exports.deleteBooks = async (req, res) => {
+  // books = [];
+  try {
+    await Book.deleteMany();
+    res.send("All books deleted");
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
 
 // FILTERS
 
 // GET by filter  /books?read=true
-exports.getBooksByFilter = (req, res) => {
-    const { title, author, read } = req.query;
-    let filteredBooks = books;
-  
-    if (read) {
-      filteredBooks = filteredBooks.filter(book => book.read === read);
-    }
-    if (title) {
-      filteredBooks = filteredBooks.filter(book => book.title === title);
-    }
-    if (author) {
-      filteredBooks = filteredBooks.filter(book => book.author === author);
-    }
-    res.status(200).json(filteredBooks);
-  };
+// exports.getBooksByFilter = (req, res) => {
+//   const { title, author, read } = req.query;
+//   let filteredBooks = books;
+
+//   if (read) {
+//     filteredBooks = filteredBooks.filter((book) => book.read === read);
+//   }
+//   if (title) {
+//     filteredBooks = filteredBooks.filter((book) => book.title === title);
+//   }
+//   if (author) {
+//     filteredBooks = filteredBooks.filter((book) => book.author === author);
+//   }
+//   res.status(200).json(filteredBooks);
+// };
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
-const port = 3000;
-const router = require('./router.js');
+const port = process.env.PORT || 3000;
+const router = require("./router.js");
+const mongoose = require("mongoose");
+
+// prettier-ignore
+// Connect to MongoDB Atlas using the DB_URI environment variable
+try {
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log("Connected to MongoDB Atlas");
+} catch (error) {
+  console.log(error);
+}  
 
 // Tell the Express application to parse incoming JSON data
 app.use(express.json());
 
 // Tell the Express application to use the router we defined in router.js
-app.use('/api', router);
+app.use("/api", router);
 
 // Start the server and listen for incoming requests on the specified port
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
